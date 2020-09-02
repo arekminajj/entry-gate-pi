dea86777f7a3a55b48681e4cc7fa7c307185d1b0
Arek Cios <arek123113@gmail.com>
2020-09-02 20:44:31 +0200

    Add try/except to client

--- client.py
+++ client.py
@@ -23,27 +23,32 @@ def updateLocalJson(is_closed, should_be_closed):
     with open('status.json', 'w') as json_file:
         json.dump(statusDict, json_file)
 
-while(True):    
-    time.sleep(1)
-    response = requests.get(url)
-    status = response.json()
-    print(status['IsClosed'])
-    print(status['ShouldBeClosed'])
+while(True):
+    try:
+        time.sleep(1)
+        response = requests.get(url)
+        status = response.json()
+        print(status['IsClosed'])
+        print(status['ShouldBeClosed'])
 
-    with open('status.json') as json_file:
-        data = json.load(json_file)
+        #with open('status.json') as json_file:
+        #    data = json.load(json_file)
         isclosed = status['IsClosed']
         shouldbeclosed = status['ShouldBeClosed']
 
-    if isclosed == True and shouldbeclosed == True:
-        print("Gate is closed")
-    elif isclosed == True and shouldbeclosed == False:
-        gate.openclose(21) # OPEN THE GATE!
-        print("Gate is opening")
-        updateApiJson(False, shouldbeclosed)
-    elif isclosed == False and shouldbeclosed == False:
-        print("Gate is open")
-    elif isclosed == False and shouldbeclosed == True:
-        gate.openclose(21) #CLOSE DA GATE!
-        print("Gate is closing")
-        updateApiJson(True, shouldbeclosed)
+        if isclosed == True and shouldbeclosed == True:
+            print("Gate is closed")
+        elif isclosed == True and shouldbeclosed == False:
+            gate.openclose(21) # OPEN THE GATE!
+            print("Gate is opening")
+            updateApiJson(False, shouldbeclosed)
+        elif isclosed == False and shouldbeclosed == False:
+            print("Gate is open")
+        elif isclosed == False and shouldbeclosed == True:
+            gate.openclose(21) #CLOSE DA GATE!
+            print("Gate is closing")
+            updateApiJson(True, shouldbeclosed)
+    except:
+        print("Something went wrong, retrying...")
+        continue
+    
