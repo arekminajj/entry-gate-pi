ae3fc6fc481b6903ca22cdcfa647e1b050e60a77
Arek Cios <arek123113@gmail.com>
2020-09-02 19:24:10 +0200

    Fix gate.py

--- gate.py
+++ gate.py
@@ -3,11 +3,11 @@ import time
 
 channel = 21
 
-#GPIO SETUP
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(channel, GPIO.OUT)
-
 def openclose(pin):
+        #GPIO SETUP
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(channel, GPIO.OUT)
+
         GPIO.output(pin, GPIO.HIGH)
         time.sleep(0.3)
         GPIO.output(pin, GPIO.LOW)
